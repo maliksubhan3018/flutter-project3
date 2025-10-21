5940edb282d3935c4abc561debb0afa440e5fdc8
maliksubhan3018 <malik.subhan3018@gmail.com>
2025-10-21 22:45:36 +0500

    lottie animation

--- lib/custom_imageslist/my_listimages.dart
+++ lib/custom_imageslist/my_listimages.dart
@@ -3,6 +3,7 @@ class MyImages {
   static const String burger = 'assets/figma_images/Image.png';
   static const String person1 = 'assets/figma_images/Mask group.png';
   static const String starbucks = 'assets/figma_images/Image (1).png';
-  
+  static const String circleanim = 'asset/figma_images/anime.json';
+
   // Member Functions
 }
--- lib/myhero.dart
+++ lib/myhero.dart
@@ -2,19 +2,39 @@ import 'package:figma_project/custom_imageslist/my_listimages.dart';
 import 'package:figma_project/myprofile.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 // import 'package:flutter/widgets.dart';
 
-class Myhero extends StatelessWidget {
+class Myhero extends StatefulWidget {
   const Myhero({super.key});
 
+  @override
+  State<Myhero> createState() => _MyheroState();
+}
+
+class _MyheroState extends State<Myhero> {
+  bool anim=true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          
           children: [
-            Hero(tag: "developer",
+            // Lottie.asset(MyImages.circleanim, width: 200),
+            // Lottie.asset('assets/figma_images/anime.json'),
+            Lottie.asset('assets/figma_images/construction.json',width: 250,),
+            SizedBox(height: 20,),
+TextButton(onPressed:(){
+  anim=!anim;
+  setState(() {
+    
+  });
+} , child: Text("mybutton")),
+            SizedBox(height: 20,)
+,            // Image.asset('assets/figma_images/image.png'),
+            Hero(
+              tag: "developer",
               child: ListTile(
                 onTap: () {
                   Get.to(() => Myprofile());
